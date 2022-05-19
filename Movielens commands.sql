ad07497d35cbb8a8636bc07ae9da5dd07cf5b85b
Tyron-P <105211259+Tyron-P@users.noreply.github.com>
2022-05-19 17:03:05 +0100

    Add files via upload

--- Movielens commands.sql
+++ Movielens commands.sql
@@ -1,9 +1,28 @@
 Select title, Release_date from movies where release_date between  "1983-01-01" and "1993-12-31" order by release_date desc;
 
 select release_date from movies;
+#--------------------------------
 
 select rating from ratings;
 
 Select avg(rating) from ratings;
 
-Select m.title from movies m 
+Select m.title, r.rating from movies m join ratings r on m.id=r.id;
+
+select title, `movie_id` from movies m join
+(select movie_id, avg(rating)
+from ratings group by movie_id) lw on m.id=lw.movie_id where `movie_id` <=3;
+
+Select title, `avg(rating)` from movies m join 
+(select movie_id, avg(rating) 
+from ratings group by movie_id) averages on m.id=averages.movie_id where `avg(rating)`<=1;
+
+
+#-------------------------
+Select name from genres;
+Select name from genres where name='Sci-Fi';
+select name from occupations where name="student";
+Select age, gender from users where age=24 and gender="m";
+
+#-----------
+select genre_id
